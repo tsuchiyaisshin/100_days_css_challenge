85586898b9ae18a43fea1fbf3279a5291fe6335c
tsuchiya_isshin <tuchiyaisshin@icloud.com>
2021-02-12 12:08:38 +0900

    feat: add animation

--- day1/index.js
+++ day1/index.js
@@ -1,4 +1,6 @@
-$('.menu-icon').bind('click', function() {
-    $(this).toggleClass('active');
-    $(this).find('div').removeClass('no-animation');
+$(document).ready(function() {
+    $('.menu-icon').bind('click', function () {
+        $(this).toggleClass('active');
+        $(this).find('div').removeClass('no-animation');
+    });
 });
