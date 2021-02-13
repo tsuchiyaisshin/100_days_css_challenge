74ad36d01ebcaa3ef6e236abcf27ce6ff8e24696
tsuchiya_isshin <tuchiyaisshin@icloud.com>
2021-02-13 17:59:55 +0900

    create day 2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# 100_days_css_challenge
+# 100 Days CSS Challenge
+毎日がeveryday!!!!!!!!!!!!!!!!!!
+
+##Compile SCSS tp CSS
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+<br>
+`sass test.scss:test.css`
---
+++ day2/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu-icon').bind('click', function () {
+        $(this).toggleClass('active');
+        $(this).find('div').removeClass('no-animation');
+    });
+});
